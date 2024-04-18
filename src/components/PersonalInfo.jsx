10d17ea6aa4dac0422dc396580ca144bcc87bed6
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-04-18 09:50:45 +0100

    Add styling to personal information component add mobile design to personal info component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './PersonalInfo.css';
 
 export default function PersonalInfo() {
     const [personalInfo, setPersonalInfo] = useState({
@@ -28,60 +29,54 @@ export default function PersonalInfo() {
         }
     }
 
-    function handlePhone(event) {}
-
     return (
         <div className="personal-wrapper">
-            <form action="">
-                <h2>Personal Information</h2>
-                <div>
-                    <div>
-                        <label htmlFor="first-name">First name</label>
-                        <input
-                            type="text"
-                            id="first-name"
-                            value={personalInfo.firstName}
-                            onChange={(event) => handleChange(event, 'FName')}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="last-name">Last name</label>
-                        <input
-                            type="text"
-                            id="last-name"
-                            value={personalInfo.lastName}
-                            onChange={(event) => handleChange(event, 'LName')}
-                        />
-                    </div>
+            <form className="personal-form" action="">
+                <h2 className="form-heading">Personal Information</h2>
+                <div className="input-group">
+                    <label htmlFor="first-name">First name</label>
+                    <input
+                        type="text"
+                        id="first-name"
+                        value={personalInfo.firstName}
+                        onChange={(event) => handleChange(event, 'FName')}
+                    />
                 </div>
-                <div>
-                    <div>
-                        <label htmlFor="position">Position</label>
-                        <input
-                            type="text"
-                            id="position"
-                            value={personalInfo.position}
-                            onChange={(event) => handleChange(event, 'position')}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email Address</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={personalInfo.emailAddress}
-                            onChange={(event) => handleChange(event, 'email')}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="phone">Phone No</label>
-                        <input
-                            type="tel"
-                            id="phone"
-                            value={personalInfo.phoneNumber}
-                            onChange={(event) => handleChange(event, 'phone')}
-                        />
-                    </div>
+                <div className="input-group">
+                    <label htmlFor="last-name">Last name</label>
+                    <input
+                        type="text"
+                        id="last-name"
+                        value={personalInfo.lastName}
+                        onChange={(event) => handleChange(event, 'LName')}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="position">Position</label>
+                    <input
+                        type="text"
+                        id="position"
+                        value={personalInfo.position}
+                        onChange={(event) => handleChange(event, 'position')}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={personalInfo.emailAddress}
+                        onChange={(event) => handleChange(event, 'email')}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="phone">Phone No</label>
+                    <input
+                        type="tel"
+                        id="phone"
+                        value={personalInfo.phoneNumber}
+                        onChange={(event) => handleChange(event, 'phone')}
+                    />
                 </div>
             </form>
         </div>
